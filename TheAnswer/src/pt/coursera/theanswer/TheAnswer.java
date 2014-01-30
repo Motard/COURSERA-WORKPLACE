031f78638b4223a9e167d7338037796f202b8488
Paulo <pmotard@sapo.pt>
2014-01-30 11:27:42 +0000

    acertos

--- TheAnswer/src/pt/coursera/theanswer/TheAnswer.java
+++ TheAnswer/src/pt/coursera/theanswer/TheAnswer.java
@@ -1,13 +1,16 @@
 package pt.coursera.theanswer;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.widget.TextView;
+import android.util.Log;
 
 public class TheAnswer extends Activity {
 
+	private final String TAG = "TheAnswer"; 
 	public static final int[] answers = {42, -10, 0, 100, 1000};
 	public static final int answer = 42;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,12 +18,14 @@ public class TheAnswer extends Activity {
         
         TextView answerView = (TextView) findViewById(R.id.answer_view);
         
+        Log.i(TAG, "Printing the answer to life");
+        
         int val = findAnswer();
         String output = (val == answer)? "42" : "We may never know.";
         answerView.setText("The answer to Life, The Universe and Everything is: \n\n" + output);
     }
 
-
+    
     private int findAnswer()
     {
     	for (int val : answers)
